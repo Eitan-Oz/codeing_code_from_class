0c450d59b3342d7a88f97063f5053f97a139060b
student <student@DESKTOP-305FGH>
2025-09-08 13:40:29 +0300

    Add Toy class with price management functionality
    Updated `Leern_inheritance.csproj` to include `Toy.cs`.
    Introduced a new `Toy` class with fields for price and color,
    along with methods to retrieve and update the price based
    on a percentage.

---
+++ Leern_inheritance/Toy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Toy
+    {
+
+        protected double price;
+        protected string color;
+      
+        public Toy(int price, string color)
+        {
+            this.price = price;
+            this.color = color;
+        }
+
+        public double Price()
+        {
+            return price;
+        }
+
+        public void UpdatePrice(double percent)
+        {
+            price*= ((100 +  percent) / 100) ;
+        }
+    }
+}
