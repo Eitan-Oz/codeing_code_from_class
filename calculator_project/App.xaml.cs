3e6c927305002a2fc82483561c8e37d4ffe80a23
Inbar Bachar <Inbar Bachar@LAPTOP-CVGVFKL7>
2025-09-10 12:08:38 +0300

    Update project to .NET MAUI with multi-platform support
    This commit introduces .NET MAUI to the project, enabling support for Android, iOS, Mac Catalyst, and Windows. Key changes include the creation of `App.xaml` and `App.xaml.cs` for application resources, `AppShell.xaml` for navigation structure, and `MainPage.xaml` for the main user interface.
    
    Additional files such as `MauiProgram.cs`, `AndroidManifest.xml`, and `Info.plist` have been added or modified for platform-specific configurations. Resource files like `colors.xml` and `styles.xaml` have been included to enhance the visual appearance, and the project file has been updated to reference necessary packages and target frameworks. The solution file has also been updated to reflect the new project structure.
    and delete the Leern_inheritance project

---
+++ calculator_project/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace calculator_project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
