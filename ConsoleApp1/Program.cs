304e87ff229b7fed6fad03c7aae2e592641b93d2
Inbar Bachar <Inbar Bachar@LAPTOP-CVGVFKL7>
2025-09-11 15:27:53 +0300

    Remove random number generation from Main method
    Eliminate the loop that generated and displayed random numbers in various colors. The `Random` instance and associated logic have been removed, resulting in a simplified `Main` method.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,24 +9,9 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        public static Random random = new Random();
         static void Main(string[] args)
         {
-            for (int i = int.MinValue; i < int.MaxValue; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(random.Next(0, 2));
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(random.Next(0, 2));
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(random.Next(0, 2));
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write(random.Next(0, 2));
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(random.Next(0, 2));
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write(random.Next(0, 2));
-            }
+
         }
     }
 }
