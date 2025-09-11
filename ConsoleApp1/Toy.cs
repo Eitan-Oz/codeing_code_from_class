66dde0ed6871bcfa8fa98844e6e99462989965a7
Inbar Bachar <Inbar Bachar@LAPTOP-CVGVFKL7>
2025-09-11 14:40:43 +0300

    Add new classes and update project files
    Updated `ConsoleApp1.csproj` to include `Car.cs`, `Doll.cs`, and `Toy.cs`. Modified the solution file to add a new project "text files from class". Created three new classes in the `ConsoleApp1` namespace, with `Toy` containing protected fields for `name` and `price`.

---
+++ ConsoleApp1/Toy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Toy
+    {
+        protected string name;
+        protected decimal price;
+
+    }
+}
